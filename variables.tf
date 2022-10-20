666309db048ce388d1ac0c541dd1b2421abc894b
yuheejae <yuheejae@lguplus.co.kr>
2022-10-20 14:13:39 +0900

    [chapter7] hands on - 변수값 추가

--- variables.tf
+++ variables.tf
@@ -37,9 +37,8 @@ variable "placeholder" {
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
 
-
 variable "environment" {
-  type = "string"
+  type = string
   default = "dev"
   description = "Define infrastructure's enviroment"
   validation {
